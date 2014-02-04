9b8d41d338d9397541df9d4f7e73eef5d10212d1
Justin M. Keyes <justinkz@gmail.com>
2014-02-04 01:44:34 -0500

    emacs: speedbar: do not wrap

--- .emacs.d/config/init-speedbar.el
+++ .emacs.d/config/init-speedbar.el
@@ -1,6 +1,10 @@
 (require-package 'sr-speedbar)
 (require 'sr-speedbar)
 
+(setq speedbar-show-unknown-files t)
 (sr-speedbar-refresh-turn-off)
+
+(add-hook 'speedbar-mode-hook
+          (lambda () (setq truncate-lines t)))
 
 (provide 'init-speedbar)
