4c5bc874ebcb7bb2c29fecbb10f13378c10ecc38
Justin M. Keyes <justinkz@gmail.com>
2015-01-27 21:32:43 -0500

    emacs: give up on explicit-shell-file-name = bash

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -69,12 +69,6 @@
 (with-package exec-path-from-shell ;; make sure $PATH is set correctly
   (ignore-errors ;; windows
     (exec-path-from-shell-initialize)))
-(pcase (window-system)
-  (`w32 (progn 
-          (setq explicit-shell-file-name "C:/Program Files (x86)/Git/bin/bash")
-          (setq explicit-bash-args '("--login" "-i"))
-          (setq shell-file-name explicit-shell-file-name)
-          (add-to-list 'exec-path "C:/Program Files (x86)/Git/bin/"))))
 
 ;; editor
 (with-package* undo-tree
