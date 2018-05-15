912d6fa22c0e9a716478ce4ae356c58674828f84
Justin M. Keyes <justinkz@gmail.com>
2018-05-15 16:57:12 +0200

    bin/gist.py: authenticate via .netrc

--- bin/gist.py
+++ bin/gist.py
@@ -1,23 +1,34 @@
 from sys import stdin
 import json
 import requests
+import netrc
 
-inputstr = ""
-for line in stdin:
-    inputstr = inputstr + line
+def main_():
+    netrc_info = netrc.netrc()
+    gist_api_cred = netrc_info.authenticators('gist.github.com')
+    gist_api_token = gist_api_cred[0]
 
-gist_api_request_payload = {
-  "description": "",
-  "public": False,
-  "files": {
-    "file.txt": {
-      "content": inputstr
+    inputstr = ""
+    for line in stdin:
+        inputstr = inputstr + line
+
+    gist_api_request_payload = {
+      "description": "",
+      "public": False,
+      "files": {
+        "unnamed.txt": {
+          "content": inputstr
+        }
+      }
     }
-  }
-}
 
-response = requests.post("https://api.github.com/gists",
-                    data=json.dumps(gist_api_request_payload),
-                    headers={})
+    response = requests.post("https://api.github.com/gists",
+                        data=json.dumps(gist_api_request_payload),
+                        headers={
+                            'Authorization': 'token {}'.format(gist_api_token)
+                        })
 
-print('response: ' + str(response.json()['html_url']))
+    j = response.json()
+    print('response: ' + str(response.json()['html_url']))
+
+main_()
