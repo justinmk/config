d894184842b0e54b546a240fc431a20bc754e5d5
Justin M. Keyes <justinkz@gmail.com>
2018-06-16 01:11:36 +0200

    bin/gist.py: migrate to GitLab

--- bin/gist.py
+++ bin/gist.py
@@ -2,43 +2,49 @@ from sys import stdin
 import json
 import requests
 import netrc
+import urllib
 
 DEBUG=False
 
 def main_():
     netrc_info = netrc.netrc()
-    gist_api_cred = netrc_info.authenticators('gist.github.com')
+    gist_api_cred = netrc_info.authenticators('gitlab.com')
     gist_api_token = gist_api_cred[0]
+
+    gitlab_project_id = urllib.parse.quote_plus('justinmk/gist')
 
     inputstr = ""
     for line in stdin:
         inputstr = inputstr + line
 
     gist_api_request_payload = {
-      "description": "",
-      "public": False,
-      "files": {
-        "unnamed.txt": {
-          "content": inputstr
-        }
-      }
+      'id': '{}/snippets'.format(gitlab_project_id),
+      'title': 'unnamed',
+      'file_name': 'unnamed.txt',
+      'description': '',
+      'visibility': 'private',
+      'code': inputstr,
     }
 
     if DEBUG:
         print(gist_api_request_payload)
 
-    response = requests.post("https://api.github.com/gists",
-                        data=json.dumps(gist_api_request_payload),
+    endpoint = 'https://gitlab.com/api/v4/projects/{}/snippets'.format(gitlab_project_id)
+    response = requests.post(endpoint,
+                        data=gist_api_request_payload,
                         headers={
-                            'Authorization' : 'token {}'.format(gist_api_token),
-                            'Content-Type'  : 'application/json'
+                            'Private-Token' : '{}'.format(gist_api_token),
+                            # GitLab uses form-encoding, not JSON.
+                            # 'Content-Type'  : 'application/json',
                         })
 
     j = response.json()
     try:
-        print('URL: ' + str(response.json()['html_url']))
+        j = response.json()
+        print('response: {}'.format(j))
+        print('URL: {}'.format(j['web_url']))
     except KeyError:
-        print('response: ' + str(response.json()))
+        print('error: web_url key not in response')
 
 if __name__ == '__main__':
     main_()
